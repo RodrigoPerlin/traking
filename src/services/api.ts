68afb6561d533abb56f663ea68f35d380a866ed9
Rodrigo Perlin <roperlin.gok@rd.com.br>
2023-10-29 05:58:51 -0300

    feat(api): add urls

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import { ENVIRONMENT } from '../utils/urls'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: ENVIRONMENT,
 })
 
 export default api
